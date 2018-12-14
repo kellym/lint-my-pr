1315cf3bc8ba3a06d74b08d2d06817d199664529
Kelly <kellymartinv@gmail.com>
2018-12-14 10:34:23 -0600

    multiple-add-and-subtract

--- test/fixtures/multiple-add-and-subtract.js
+++ test/fixtures/multiple-add-and-subtract.js
@@ -1,13 +1,15 @@
 const daysOfSun = 250,
-      foods = ['tzatziki','feta','gyros'],
-      islands = { inhabited: 170, total: 2000}
+      islands = { inhabited: 170, total: 2000},
+      weddingEvents = ['plate-throwing','dancing' ];
 
 function getFactAboutCountry(fact) {
   switch(fact) {
     case 'food':
-      return foods[1];
+      return foods[0];
     case 'islands':
-      return islands.inhabited
+      return islands.total;
+    case 'wedding':
+      return weddingEvents[0]
     default:
       return "These are fun facts!";
   }
