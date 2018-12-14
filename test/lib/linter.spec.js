7983c494d5b7121d3ecfd946af297cac93e980a9
Kelly <kellymartinv@gmail.com>
2018-12-14 11:28:49 -0600

    tests

--- test/lib/linter.spec.js
+++ test/lib/linter.spec.js
@@ -16,18 +16,86 @@ test('Linter', t => {
 
   test('compileComments', t => {
 
-    test('correctly assigns positions in diff for add', t => {
-      const addDiff = fs.readFileSync(path.resolve('./test/fixtures/add.diff'));
+    test('diff for add', t => {
+      const file = 'test/fixtures/add';
+      setup();
+      const addDiff = fs.readFileSync(path.resolve(`./${file}.diff`), 'utf8');
       linter.diff = parse(addDiff);
-      linter.path = './test/fixtures/add.js';
+      linter.path = `./${file}.js`;
       linter.setup();
       linter.lint();
-
       linter.compileComments();
-
+      const comments = linter.fileComments[`${file}.js`];
+      t.deepLooseEqual(comments, {
+        1: ['\'daysOfSun\' is assigned a value but never used.'],
+        2: [
+          'A space is required after \',\'.',
+          'A space is required after \',\'.'
+        ],
+        3: ['A space is required before \'}\'.', 'Missing semicolon.'],
+        5: ['\'getFactAboutCountry\' is defined but never used.'],
+        10: ['Missing semicolon.'],
+        12: ['Strings must use singlequote.']
+      }, 'correctly assigns positions');
       t.end();
-
     });
+
+    test('diff for subtract', t => {
+      const file = 'test/fixtures/subtract';
+      setup();
+      const addDiff = fs.readFileSync(path.resolve(`./${file}.diff`), 'utf8');
+      linter.diff = parse(addDiff);
+      linter.path = `./${file}.js`;
+      linter.setup();
+      linter.lint();
+      linter.compileComments();
+      const comments = linter.fileComments[`${file}.js`];
+      t.looseEqual(comments, {}, 'ignores errors on subtracted lines');
+      t.end();
+    });
+
+    test('diff for add and subtract', t => {
+      const file = 'test/fixtures/add-and-subtract';
+      setup();
+      const addDiff = fs.readFileSync(path.resolve(`./${file}.diff`), 'utf8');
+      linter.diff = parse(addDiff);
+      linter.path = `./${file}.js`;
+      linter.setup();
+      linter.lint();
+      linter.compileComments();
+      const comments = linter.fileComments[`${file}.js`];
+      t.deepLooseEqual(comments, {
+        4: ['A space is required before \'}\'.'],
+        5: [
+          '\'weddingEvents\' is assigned a value but never used.',
+          'A space is required after \',\'.',
+          'There should be no space before \']\'.'
+        ]
+      }, 'correctly assigns positions after additions and subtractions');
+      t.end();
+    });
+
+    test('diff for add and subtract multiple times', t => {
+      const file = 'test/fixtures/multiple-add-and-subtract';
+      setup();
+      const addDiff = fs.readFileSync(path.resolve(`./${file}.diff`), 'utf8');
+      linter.diff = parse(addDiff);
+      linter.path = `./${file}.js`;
+      linter.setup();
+      linter.lint();
+      linter.compileComments();
+      const comments = linter.fileComments[`${file}.js`];
+      t.deepLooseEqual(comments, {
+        4: ['A space is required before \'}\'.'],
+        5: [
+          'A space is required after \',\'.',
+          'There should be no space before \']\'.'
+        ],
+        16: ['Missing semicolon.']
+      }, 'correctly assigns positions after multiple add and subtracts');
+      t.end();
+    });
+
 
     t.end();
 
